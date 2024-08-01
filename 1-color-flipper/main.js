d90e5363006598ddf8f307b991d5193ee7350263
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-08-01 13:26:12 +0200

    wip fetch color name from api

--- 1-color-flipper/main.js
+++ 1-color-flipper/main.js
@@ -1,18 +1,20 @@
 const main = document.getElementsByTagName('main')[0];
 const colorHex = document.getElementById('color-hex');
+const colorName = document.getElementById('color-name');
 
 const btn = document.getElementById('btn-new-color');
 btn.addEventListener('click',onClick);
 
 function onClick(){
-    const newColor = getNewColorCode();
+    const newColor = generateNewColorCode();
     console.log(newColor);
-    colorHex.innerText = newColor;
-    main.style.backgroundColor = newColor;
+    colorHex.innerText = `#${newColor}`;
+    main.style.backgroundColor = `#${newColor}`;
+    colorName.innerText = getColorNameFromApi(newColor);
 }
 
-function getNewColorCode(){
-    let colorRGB = '#';
+function generateNewColorCode(){
+    let colorRGB = '';
     // R -> 0, G -> 1, B -> 2
     for(let i=0; i<3; i++){
         const decimalPart = Math.floor(Math.random() * 225) + 1;
@@ -22,3 +24,39 @@ function getNewColorCode(){
     }
     return colorRGB;
 }
+
+async function getColorNameFromApi(newColor){
+    const apiURL = `https://www.thecolorapi.com/id?hex=${newColor}`;
+    try {
+        const response = await fetch(apiURL);
+        if (!response.ok) {
+            throw new Error('Color not found');
+        }
+        const data = await response.json();
+        console.log(data.name.value);
+        return data.name.value;
+    } catch (error) {
+        console.error(error);
+        return 'no name found';
+    }
+}
+
+/*
+function getColorNameFromApi(newColor){
+    const apiURL = `https://www.thecolorapi.com/id?hex=${newColor}`;
+    return fetch(apiURL)
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Color not found');
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data.name.value);
+        return data.name.value;
+    })
+    .catch(error => {
+        console.error(error);
+        return 'no name found';
+    });
+}*/
